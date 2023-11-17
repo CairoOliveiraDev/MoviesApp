c5989a4c3b2e72ad4488f0b7ee8a4d8ceb20cf42
CairoOliveiraDev <cairo.aco@hotmail.com>
2023-11-17 13:34:39 -0300

    Initial Commit

---
+++ MoviesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesApp
+//
+//  Created by Cairo Oliveira on 17/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
